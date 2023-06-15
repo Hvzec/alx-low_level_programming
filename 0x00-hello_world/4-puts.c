a17efe23647471c26f2980abefbfef1371a0826b
hvzec <moa130719@gmail.com>
2023-06-15 20:08:07 +0100

    wow fixed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,11 @@
 
 /**
  * main - print the phrase "Programming is like building a multilingual puzzle"
- * 
  * Return: always 0 (Success)
  */
 int main(void)
 {
 	    puts("\"Programming is like building a multilingual puzzle");
+	    
 	    return (0);
 }
