e46fcf8e89c64098d8a510709b4d14d90687ea16
hvzec <moa130719@gmail.com>
2023-06-15 23:52:52 +0100

    Lastadv

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <unistd.h>
+#include<unistd.h>
 
 /**
  *main - "print and that piece of art is useful - Dora Korpar, 2015-10-19"
@@ -6,7 +6,6 @@
 */
 int main(void)
 {
-	const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2, message, sizeof(message) - 1);
+	write(2, "and that piece of art is useful\" - Dora Korpar , 2015-10-19\n" 59);
 	return (1);
 }
