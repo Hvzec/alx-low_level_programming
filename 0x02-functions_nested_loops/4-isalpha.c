ef657f5fc56808657691adb686e1062c39a8ea49
hvzec <moa130719@gmail.com>
2023-06-20 15:29:37 +0100

    Extra Task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,11 +4,11 @@
  * _isalpha - checks alphabetic character.
  * @c: character checked
  * Description: check for alphabetic character.
- * Return: 1 if the character is a letter 'lowercase or uppercase', 0 otherwise.
+ * Return: 1 if the character is letter 'lowercase or uppercase', 0 otherwise.
  */
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >='A' && c <='Z'))
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
 	else
 		return (0);
