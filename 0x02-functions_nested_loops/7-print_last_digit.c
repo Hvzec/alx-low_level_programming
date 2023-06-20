d8244d9f92d36da1c359ab417cc63654de581bfe
hvzec <moa130719@gmail.com>
2023-06-20 20:27:55 +0100

    Push Task

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Its prints the last digit of a num.
+ * @n: The num 'number' to form the last digit from.
+ * Return: The value of n.
+ */
+int print_last_digit(int n)
+{
+	int last_digit;
+
+	if (n < 0)
+		n = -n;
+	last_digit = n % 10;
+	_putchar('0' + last_digit);
+
+	return (last_digit);
+}
