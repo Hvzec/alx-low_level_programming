ab27d52ebd093414b44b35cef68af1d6c038fa7a
hvzec <moa130719@gmail.com>
2023-06-20 22:25:36 +0100

    wow

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,9 +9,14 @@ void jack_bauer(void)
 	int hour, minute;
 	for (hour = 0; hour < 24; hour++)
 	{
-		for (minute = 0; minute < 66; minute++)
+		for (minute = 0; minute < 60; minute++)
 		{
-			printf("%02d:%02d\n", hour, minute);
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+			_putchar('\n');
 		}
 	}
 }
