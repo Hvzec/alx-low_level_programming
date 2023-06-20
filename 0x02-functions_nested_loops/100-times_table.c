a01188607915e9e8a7b0d7e5c4b47c087c2be7fc
hvzec <moa130719@gmail.com>
2023-06-20 23:51:04 +0100

    Alotof numbers"

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_times_table - It's printing the n times Table.
+ * @n: Tthis is the num of times.
+ */
+void print_times_table(int n)
+{
+	int i, j, result;
+
+	if (n < 0 || n > 15)
+		return;
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <=n; j++)
+		{
+			result = i * j;
+			if (j != 0)
+				printf(", ");
+			printf("%-3d", result);
+		}
+		printf("\n");
+	}
+}
