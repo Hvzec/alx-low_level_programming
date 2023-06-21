6fc8344510dcd45f9085e61b2f483e6fd3685ef4
hvzec <moa130719@gmail.com>
2023-06-20 17:52:08 -0700

    v2

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,72 +1,27 @@
 #include <stdio.h>
+
 /**
- * main -  Its finding and printing the sum of the even-valued terms.
- * Return: Always 0.
- */
-int main() {
-    int term1 = 1;
-    int term2 = 2;
-    int nextTerm = 0;
-    int sum = 2;  // Start with 2 : first even-valued term
+  * main - Its printing the sum of Even Fibonacci numbers.
+  *
+  * Return: Always 0.
+  */
+int main(void)
+{
+	int i = 0;
+	long j = 1, k = 2, sum = k;
 
-    while (nextTerm <= 4000000) {
-        nextTerm = term1 + term2;
+	while (k + j < 4000000)
+	{
+		k += j;
 
-        if (nextTerm % 2 == 0) {
-            sum += nextTerm;
+		if (k % 2 == 0)
+			sum += k;
 
-            // Printing each digit of the even term
-            int temp = nextTerm;
-            int digitCount = 0;
+		j = k - j;
 
-            // Counting the numbers of digits
-            while (temp > 0) {
-                temp /= 10;
-                digitCount++;
-            }
+		++i;
+	}
 
-            // Converting and printing each digit
-            temp = nextTerm;
-            for (int i = digitCount - 1; i >= 0; i--) {
-                int divisor = 1;
-                for (int j = 0; j < i; j++) {
-                    divisor *= 10;
-                }
-                int digit = temp / divisor;
-                putchar('0' + digit);
-                temp %= divisor;
-            }
-            putchar(' ');
-        }
-
-        term1 = term2;
-        term2 = nextTerm;
-    }
-
-    putchar('\n');
-
-    // Printing the sum
-    int temp = sum;
-    int digitCount = 0;
-
-    // Counting the number of digits
-    while (temp > 0) {
-        temp /= 10;
-        digitCount++;
-    }
-
-    // Converting and printing each digit
-    temp = sum;
-    for (int i = digitCount - 1; i >= 0; i--) {
-        int divisor = 1;
-        for (int j = 0; j < i; j++) {
-            divisor *= 10;
-        }
-        int digit = temp / divisor;
-        putchar('0' + digit);
-        temp %= divisor;
-    }
-    putchar('\n');
-
-    return (0);
+	printf("%ld\n", sum);
+	return (0);
 }
