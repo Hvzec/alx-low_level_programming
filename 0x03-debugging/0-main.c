140e21b0d68b194fb7883dae5b99faf23420476d
hvzec <moa130719@gmail.com>
2023-06-21 21:07:09 +0100

    -Lower ;

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - It's Testing the function that prints if the int is (+),(-) or is (zero)
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+
+	positive_or_negative(i);
+
+	return (0);
+}
